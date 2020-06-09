84af3639cee6c16645c05afda353854e6df27cb1
Ubuntu <ubuntu@ip-172-31-31-168.eu-west-1.compute.internal>
2020-06-09 07:46:11 +0000

    Jenkins

--- docker/jenkins/docker.sh
+++ docker/jenkins/docker.sh
@@ -1,3 +1,7 @@
 docker pull jenkins/jenkins:lts
-docker container create --name mijenkins -v /home/ubuntu/environment/curso/docker/jenkins/data:/var/jenkins_home -p 8080:8080 jenkins/jenkins:lts
+docker container create \
+    --name mijenkins \
+    -v /home/ubuntu/environment/curso/docker/jenkins/data:/var/jenkins_home \
+    -p 8080:8080 \
+    jenkins/jenkins:lts
 docker start mijenkins
