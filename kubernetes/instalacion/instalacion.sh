87669688226804b439410a328b599f36fa7f2d71
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2020-07-17 09:23:22 +0200

    Update instalacion.sh

--- kubernetes/instalacion/instalacion.sh
+++ kubernetes/instalacion/instalacion.sh
@@ -1,12 +1,18 @@
 # Requisitos
 
-# R1: docker <- cgroups NO -> SI systemd
-#  --exec-opt native.cgroupdriver=systemd en dockerd al arrancar servicio de docker
+# R1: docker
+# R1.1: docker configurado no con groups-> systemd
+systemctl status docker
+sudo vim /lib/systemd/system/docker.service
+## Añadir --exec-opt native.cgroupdriver=systemd en dockerd al arrancar servicio de docker
+# Recargar el servicio y reiniciarlo
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
-# R2: Desactivar swapping
-sudo swapoff -a
-sudo vim /etc/fstab
-free # comprobar con free que no hay swap
+# R2: Descativar Swapping en el SO
+sudo swapoff -a # Desactiva solo ahora
+# Comprobar con: free
+sudo vim /etc/fstab # Desactivarlo permanentemente
 
 # Pasos
 # Paso 1: Alta del repo de kubernetes
@@ -21,30 +27,26 @@ kubeadm version # comprobar la instalación
 
 # Paso 3: Crear cluster con kubeadm
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-# Hay varias lineas que debemos ejecutar qye salen como resultado del init
-#  mkdir -p $HOME/.kube
-#  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# Me salen del comando anterior:
+# Configurar mi usuario
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Para añadir otras maquinas al cluster, ejecutar: (sale también del init)
-  #kubeadm join 172.31.31.168:6443 --token badbhg.ekjdi0muuw47vca0 \
-  #    --discovery-token-ca-cert-hash sha256:79c24a7e581f18170f6076b108f49fca1383fe44ab5bf6e8a75cd0ca9250c407 
-  
-kubectl get nodes   # Compruebo que mi cluster está instalado y que tiene su maestro
-kubectl get pods    # Lista los pods del cluster los del namespace por defecto
-kubectl get pods --all-namespaces # Lista todos los pods
-kubectl get pod -n kube-system  # Listar los pods de un namespace
+# Ya tenemos disponible kubectl  
 
-# Describir un pod que tengo en el cluster
-kubectl describe pod coredns-66bff467f8-8klz5 -n kube-system       
-
-# Configurar driver de red para los pods (indicado en el init)
+# Montar una red
+#kubectl apply -f [podnetwork].yaml with one of the options listed at:
+#  https://kubernetes.io/docs/concepts/cluster-administration/addons/
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
+# Juntar nuevos nodos al cluster
+kubeadm join 172.31.8.183:6443 --token s0xdad.kmuq7psv9rtaz2jd \
+    --discovery-token-ca-cert-hash sha256:1239c23581b68f7bcaab4183eb1a71e9fd4c685605a5217695d462e77f445e5f 
+    
 # Activar autocompletado para kubernetes: https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
 echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
-
 
 # Para JUGAR y poder instalar cosas en el nodo maestro:
 kubectl taint nodes --all node-role.kubernetes.io/master-
